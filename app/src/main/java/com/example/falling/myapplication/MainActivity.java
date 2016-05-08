6d91be1cb39c60106512c4a9c4127798b5afb141
falling <zyz635857938@gmail.com>
2016-05-08 19:49:59 +0800

    去掉返回的箭头

--- app/src/main/java/com/example/falling/myapplication/MainActivity.java
+++ app/src/main/java/com/example/falling/myapplication/MainActivity.java
@@ -2,15 +2,8 @@ package com.example.falling.myapplication;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
 import com.example.falling.myapplication.fragment.RecyclerViewFragment;
 import com.github.florent37.materialviewpager.MaterialViewPager;
@@ -19,8 +12,6 @@ import com.github.florent37.materialviewpager.header.HeaderDesign;
 public class MainActivity extends AppCompatActivity {
     public static final int PAGE_COUNT = 1;
     private MaterialViewPager mViewPager;
-    private DrawerLayout mDrawer;
-    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,50 +20,34 @@ public class MainActivity extends AppCompatActivity {
 
         mViewPager = (MaterialViewPager) findViewById(R.id.materialViewPager);
 
-        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (mViewPager != null) {
+            mViewPager.getViewPager().setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
 
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawer, 0, 0);
-        mDrawer.addDrawerListener(mDrawerToggle);
+                @Override
+                public Fragment getItem(int position) {
+                    return RecyclerViewFragment.newInstance();
+                }
 
-        mViewPager.getViewPager().setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+                @Override
+                public int getCount() {
+                    return PAGE_COUNT;
+                }
 
-            @Override
-            public Fragment getItem(int position) {
-                return RecyclerViewFragment.newInstance();
-            }
-
-            @Override
-            public int getCount() {
-                return PAGE_COUNT;
-            }
-
-            @Override
-            public CharSequence getPageTitle(int position) {
-                return null;
-            }
-        });
+                @Override
+                public CharSequence getPageTitle(int position) {
+                    return null;
+                }
+            });
+        }
 
         mViewPager.setMaterialViewPagerListener(new MaterialViewPager.Listener() {
             @Override
             public HeaderDesign getHeaderDesign(int page) {
                 return HeaderDesign.fromColorResAndUrl(
-                        R.color.green,
-                        "https://fs01.androidpit.info/a/63/0e/android-l-wallpapers-630ea6-h900.jpg");
+                        R.color.red,
+                        "http://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2014/06/wallpaper_51.jpg");
             }
         });
         mViewPager.getViewPager().setOffscreenPageLimit(mViewPager.getViewPager().getAdapter().getCount());
-        mViewPager.getPagerTitleStrip().setViewPager(mViewPager.getViewPager());
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        mDrawerToggle.syncState();
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return mDrawerToggle.onOptionsItemSelected(item) ||
-                super.onOptionsItemSelected(item);
     }
 }
